72d76a6bf5d1f385630d3b3661d5a335cd6f89cc
Brian Weinreich <be.weinreich@gmail.com>
2014-11-06 17:20:39 -0500

    yo

--- pong/AppDelegate.swift
+++ pong/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         println(self.managedObjectContext)
-        
+
+
         return true
     }
 
